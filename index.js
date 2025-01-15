dc91ff33676c298b44dac641bbda1df032cf85c4
sashaknw <kaniewa.aleksandra@gmail.com>
2025-01-15 14:16:19 +0000

    done
    con bonus 1

--- index.js
+++ index.js
@@ -36,3 +36,23 @@ else if (hacker2 > hacker1) {
 } else { 
     console.log ("What?! You both have the same name?")
 }
+
+//Bonus 1
+
+const longText = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras quis laoreet quam. Cras suscipit efficitur urna a blandit. Donec risus magna, sodales a ultrices vel, imperdiet sed dolor. Nullam aliquet eleifend semper. Nunc diam nisi, finibus sed lacinia ac, ullamcorper sit amet leo. Vivamus auctor purus eget ante faucibus, sit amet malesuada eros commodo. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean interdum lectus suscipit lectus pretium, viverra lobortis mi ultrices. 
+
+Donec ornare libero quis venenatis mollis. Suspendisse potenti. Donec commodo justo sit amet accumsan consequat. Praesent eros libero, auctor ac purus non, mattis pellentesque nunc. Sed vitae lacus odio. Sed scelerisque tincidunt tellus quis venenatis. Nunc consectetur nibh non risus commodo, sodales laoreet enim commodo. Phasellus vitae sollicitudin nulla. Integer iaculis, nibh ac faucibus fringilla, nisi justo consectetur sapien, vitae placerat nunc lectus at neque. Quisque accumsan odio ante, sit amet auctor justo posuere vitae. In mollis justo nisi, id tincidunt ante hendrerit vitae. Sed pharetra odio nec cursus tincidunt.
+
+Vestibulum libero turpis, mattis eleifend nibh vel, posuere dignissim massa. Nunc convallis facilisis aliquam. Curabitur nec urna semper, pretium lorem semper, auctor velit. In hac habitasse platea dictumst. Aliquam erat volutpat. Aenean lacus mauris, semper ac vehicula sit amet, laoreet eu magna. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Pellentesque eu neque nec eros ornare feugiat. Vivamus scelerisque justo quis dapibus faucibus. Phasellus ligula ligula, dapibus id ultrices ac, mollis et nulla. Suspendisse potenti. Mauris pulvinar ex vitae orci interdum, eget scelerisque justo placerat.`;
+
+// count words
+
+const counter = longText.split(" ")
+console.log (counter.length)
+
+// count "et"
+
+const findEt = longText.indexOf("et");
+console.log(findEt);
+
+
